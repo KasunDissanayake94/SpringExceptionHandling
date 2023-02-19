254895650b3fca8f45d6577a11d8cdb873d757cb
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2023-02-19 14:02:19 +0530

    Initial commit

---
+++ src/main/java/io/reflectoring/springExceptionHandling/SpringExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.springExceptionHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reflectoring/springExceptionHandling/SpringExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.springExceptionHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
